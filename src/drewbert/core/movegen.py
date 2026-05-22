21a1ce267f0e6168b3aaa031aa1eb9e4d30863eb
drew ellingson <drew.ellingson@gmail.com>
2026-05-22 07:02:49 -0700

    draft movegen: pawn moves with ep + promotion, castling, docstrings

--- src/drewbert/core/movegen.py
+++ src/drewbert/core/movegen.py
@@ -3,17 +3,17 @@ from drewbert.core.position import Position
 from drewbert.core.types import Color, Square, Piece, PieceType
 from dataclasses import dataclass
 from typing import NamedTuple, List, Dict
-from itertools import product, chain
+from itertools import chain
 
 KNIGHT_VECTORS = [(2,1), (2,-1), (1,2), (1,-2), (-1, 2), (-1, -2), (-2, 1), (-2, -1)]
+BISHOP_UNIT_VECTORS = [(1,1), (1,-1), (-1,1), (-1, -1)]
+ROOK_UNIT_VECTORS = [(1,0), (0,1), (-1,0), (0,-1)]
+KING_VECTORS = [(1,0), (1,-1), (0,-1), (-1,-1), (-1,0), (-1,1), (0,1), (1,1)]
 
 class Coord(NamedTuple):
     file: int                                                                                                                                          
     rank: int   
 
-def get_other_color(color: Color):
-    """Color flipper"""
-    return Color.WHITE if color == color.BLACK else Color.BLACK 
 
 def get_pieces(position: Position) -> Dict[int, Piece]:
     """Return an square:piece dictionary describing pieces of the side currently to move """
@@ -55,7 +55,7 @@ def slide_along_vector(position: Position, coord:Coord, delta: Coord) -> List[Co
     while in_bounds:
         target = _add(target_squares[-1], delta)
         target_sq = position.squares[rank_file_to_sq(target)]
-        not_same_color_piece = target_sq is None or target_sq.color == get_other_color(piece.color)
+        not_same_color_piece = target_sq is None or target_sq.color == piece.color.opposite
         if coord_in_bounds(target) and not_same_color_piece:
             target_squares.append(target)
     
@@ -65,33 +65,101 @@ def slide_along_vector(position: Position, coord:Coord, delta: Coord) -> List[Co
     return target_squares
 
 def generate_pseudo_legal_knight_moves(position: Position, start_coord: Coord) -> List[Move]:
+    """
+        From a start coord, generate a list of move candidates for all valid knight moves from the coord
+        Respects out of bounds and own-piece collision
+    """
     targets = [_add(start_coord, Coord(*delta)) for delta in KNIGHT_VECTORS]
     return [Move(rank_file_to_sq(start_coord), rank_file_to_sq(target)) for target in targets]
 
 def generate_pseudo_legal_bishop_moves(position: Position, start_coord: Coord) -> List[Move]:
-    deltas = [Coord(1,1), Coord(1,-1), Coord(-1,1), Coord(-1,1)]
+    """
+        From a start coord, generate a list of move candidates for all valid bishop moves from the coord
+        Repsects out of bounds and own-piece collision 
+    """
+    deltas = [Coord(*x) for x in BISHOP_UNIT_VECTORS]
     targets = list(chain.from_iterable([slide_along_vector(position, start_coord, delta) for delta in deltas]))
     return [Move(rank_file_to_sq(start_coord), rank_file_to_sq(target)) for target in targets]
 
 def generate_pseudo_legal_rook_moves(position: Position, start_coord: Coord) -> List[Move]:
-    deltas = [Coord(1,0), Coord(-1,0), Coord(0,1), Coord(0, -1)]
+    """
+        From a start coord, generate a list of move candidates for all valid rook moves from the coord
+        Repsects out of bounds and own-piece collision 
+    """
+    deltas = [Coord(*x) for x in ROOK_UNIT_VECTORS]
     targets = list(chain.from_iterable([slide_along_vector(position, start_coord, delta) for delta in deltas]))
     return [Move(rank_file_to_sq(start_coord), rank_file_to_sq(target)) for target in targets]
 
 def generate_pseudo_legal_queen_moves(position: Position, start_coord: Coord) -> List[Move]:
+    """
+        From a start coord, generate a list of move candidates for all valid queen moves from the coord
+        Repsects out of bounds and own-piece collision 
+    """
     return generate_pseudo_legal_bishop_moves(position, start_coord) + generate_pseudo_legal_rook_moves(position, start_coord)
 
 def generate_pseudo_legal_king_moves(position: Position, start_coord: Coord) -> List[Move]:
-    coord_opts = [-1, 0, 1]
-    deltas = [Coord(*x) for x in product(coord_opts, repeat=2) if x != (0,0)]
+    """
+        From a start coord, generate a list of move candidates for all king bishop moves from the coord
+        Repsects out of bounds and own-piece collision.
+    """
+    deltas = [Coord(*x) for x in KING_VECTORS]
     targets =  [_add(start_coord, Coord(*delta)) for delta in deltas]
+
+    # Castling: squares slices represent the squares that need to be empty for castling to be legal
+    if position.side_to_move == Color.WHITE:
+        if position.castling_rights.white_kingside and all(x is None for x in position.squares[5:7]):
+            targets.append(Coord(0, 6)) # G1 
+        elif position.castling_rights.white_queenside and all(x is None for x in position.squares[1:4]):
+            targets.append(Coord(0, 2)) # C1
+    elif position.side_to_move == Color.BLACK:
+        if position.castling_rights.black_kingside and all(x is None for x in position.squares[61:63]):
+            targets.append(Coord(7,6)) # G8
+        elif position.castling_rights.black_queenside and all(x is None for x in position.squares[57:60]):
+            targets.append(Coord(7,2)) # C8
+
     return [Move(rank_file_to_sq(start_coord), rank_file_to_sq(target)) for target in targets]
 
 def generate_pseudo_legal_pawn_moves(position: Position, start_coord: Coord) -> List[Move]:
-    pass 
+    """
+        From a start coord, generate a list of move candidates for all valid pawn moves from the coord
+        Repsects out of bounds and own-piece collision
+        Handles pawn captures, en_passant, and promotion
+    """
+    dir = 1 if position.side_to_move == Color.WHITE else -1 
+    promote_rank = 7 if position.side_to_move == Color.WHITE else 0
 
+    targets = [_add(start_coord, Coord(0,dir))]
+    if start_coord.rank == 1:
+        targets = [_add(start_coord, Coord(0,dir * 2))]
+    capture_targets = [                                                                                                                                          
+        target                                                                                                                                                 
+        for x in [Coord(1, dir), Coord(-1, dir)]                                                                                                                     
+        if coord_in_bounds(target := _add(x, start_coord))                                                                                                       
+        and (target_piece := position.squares[rank_file_to_sq(target)]) is not None
+        and target_piece.color == position.side_to_move.opposite 
+    ]            
+    targets.extend(capture_targets)
+    # pawn is adjacent to en_passant_target
+    if position.en_passant_target and abs(rank_file_to_sq(start_coord) - position.en_passant_target) == 1:
+        capture_target = _add(sq_to_rank_file(position.en_passant_target), Coord(0, dir))
+        targets.append(capture_target)
+
+    moves: List[Move] = []
+    for target in targets:
+        from_sq, to_sq = rank_file_to_sq(start_coord), rank_file_to_sq(target)
+        if target.rank != promote_rank:
+            moves.append(Move(from_sq, to_sq))
+        else:
+            piece_options = [PieceType.ROOK, PieceType.KNIGHT, PieceType.BISHOP, PieceType.QUEEN]
+            moves.extend([Move(from_sq, to_sq, piece) for piece in piece_options])
+
+    return moves 
 
 def generate_piece_pseudo_legal_moves(position: Position, piece: Piece, start_coord: Coord):
+    """
+        Given a piece and a start coord, fine all pseudo-legal moves
+        Respects out of bounds and piece collision 
+    """
     movers = {
         PieceType.KNIGHT: generate_pseudo_legal_knight_moves,
         PieceType.BISHOP: generate_pseudo_legal_bishop_moves,
@@ -101,8 +169,6 @@ def generate_piece_pseudo_legal_moves(position: Position, piece: Piece, start_co
         PieceType.PAWN: generate_pseudo_legal_pawn_moves,
     }
     return movers[piece.type](position, start_coord)
-
-
 
 
 def generate_pseudo_legal_moves(position: Position) -> list[Move]:
@@ -124,8 +190,7 @@ def is_square_attacked(position: Position, target_square: Square, by: Color) -> 
 def is_in_check(position: Position, color: Color) -> bool:
     """True iff the king of `color` is currently attacked."""
     king_pos = min(i for i,x in enumerate(position.squares) if x and x.type == PieceType.KING and x.color == color)
-    return is_square_attacked(position, king_pos, get_other_color(color))
-
+    return is_square_attacked(position, king_pos, color.opposite)
 
 
 def generate_legal_moves(position: Position) -> list[Move]:
@@ -134,4 +199,5 @@ def generate_legal_moves(position: Position) -> list[Move]:
     A move is legal iff it is pseudo-legal AND does not leave the moving
     side's king in check.
     """
-    raise NotImplementedError("phase 1")
+    candidate_moves = generate_pseudo_legal_moves(position)
+    raise NotImplementedError('Phase 1')
