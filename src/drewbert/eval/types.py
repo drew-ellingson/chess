7f702578eda6f8b9e1f07a85f68bdcbc4da3ce7a
drew ellingson <drew.ellingson@gmail.com>
2026-06-12 16:59:57 -0700

    make ruff happy

--- src/drewbert/eval/types.py
+++ src/drewbert/eval/types.py
@@ -1,6 +1,7 @@
-from typing import Callable
+from collections.abc import Callable
+
+from drewbert.core.move import Move
 from drewbert.core.position import Position
-from drewbert.core.move import Move 
 
 type PositionEvalFn = Callable[[Position], int]
 type MoveEvalFn = Callable[[Position, Move], int]
